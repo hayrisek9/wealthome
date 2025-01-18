9cdfb60df2c50462eda76a82c58f5a2f2eecd2db
hayrisek9 <hayrisek9@gmail.com>
2025-01-18 10:14:32 +0100

    properties created

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,11 @@ const /**{NodeElement} */$header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove" ]("active");
 });
+/**ADD FAVORITE BUTTON TOGGLE */
+const /**{NodeElement} */$toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+    
+});
